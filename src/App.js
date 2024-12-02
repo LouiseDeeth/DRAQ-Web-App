8038e5cf49b760ee2c80a80ac724effb663f1f5f
Louise Deeth <louisedeeth@gmail.com>
2024-12-02 09:20:23 +0000

    started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/navbar';
+import Header from './components/header';
+import Footer from './components/footer';
+import Content from './components/Content';
+import Read from './components/Read';
+import Create from './components/Create';
+import Edit from './components/edit';
 
+//added links in navbar to routes
+//Updated the App so that it display the Footer component when the URL changes to localhost:3000/read 
+//and Header component when the URL of the App changes to localhost:3000/create under the Navigation bar.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<Read/>} />
+        <Route path="/create" element={<Create />} />
+        <Route path='/edit/:id' element={<Edit />} />
+      </Routes>
+      <Footer />
+    </Router>
+  ); //path='/edit/:id': The :id portion is a URL parameter that represents the unique ID of the movie the user wants to edit. When navigating to this path, React Router will capture the id from the URL and pass it to the Edit component through the useParams hook, allowing the component to fetch and update the correct movie data.
+//element={<Edit />}: This specifies that the Edit component should be rendered when the route /edit/:id is visited. By associating this route with the Edit component, the app can display the edit form for the selected movie.
 }
 
 export default App;
