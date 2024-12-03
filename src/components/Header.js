75cfbb1e0216fbb3d1fe1b297c2dfa3e2f53acc9
Louise Deeth <louisedeeth@gmail.com>
2024-12-03 21:35:01 +0000

    Added new header and logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,26 @@
-//Added a header.js file
+import '../App.css';
+import logo from '../logo.svg'; 
+
 const Header = () => {
-    return <h1>My Header in another component</h1>;
-  };
-  
-  export default Header;
+  return (
+    <h1 className="header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '20px' }}>
+      <img
+        src={logo} 
+        alt="Logo"
+        width="40" 
+        height="40" 
+        className="d-inline-block align-top"
+      />
+      My Favourite Recipes
+      <img
+        src={logo} 
+        alt="Logo"
+        width="40" 
+        height="40" 
+        className="d-inline-block align-top"
+      />
+    </h1>
+  );
+};
+
+export default Header;
