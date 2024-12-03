1416715eeb7ab8ec046f2b83d9ed33185808cee7
Louise Deeth <louisedeeth@gmail.com>
2024-12-03 21:34:07 +0000

    Added code to read the data

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,6 +1,34 @@
-import Receipes from "./Receipes";
+import Recipes from "./Recipes";
 import { useEffect, useState } from "react";
 import axios from "axios";  
 
+function Read() {
+    const [recipes, setRecipes] = useState([]);
+
+    //method to handle data reloading
+    const Reload = () => {
+      console.log("Reloading recipe data...");
+      axios.get('http://localhost:4000/api/recipes') //this line runs async
+          .then((response) => {
+            console.log(response.data);
+            setRecipes(response.data.recipe);
+          })
+          .catch((error) => {
+              console.error("Error reloading data:", error);
+          });
+    };
+  
+    useEffect(() => {
+      //reload after delete
+      Reload();
+    }, []);
+  
+    return (
+      <div>
+        <h3>Recipe List</h3>
+        <Recipes myRecipes={recipes} ReloadData={Reload}/>
+      </div>
+    );
+  }
 
 export default Read;
