abd7476b7b0a5591d4a9fb570307191012fa8dbf
Louise Deeth <louisedeeth@gmail.com>
2024-12-03 14:45:47 +0000

    Added methods for adding, finding and deleting receipes

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 4000; // port 4000 as required
+const cors = require('cors');
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //error handling to catch any server errors
 app.use((err, req, res, next) => {
@@ -8,6 +17,53 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong!');
 });
 
+app.use(express.static('public')); //middleware
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:admin@clusterdatareplab7.f2idw.mongodb.net/MyFirstDB');
+
+const recipeSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    picture: { type: String, required: true },
+    recipe: { type: String, required: true }
+});
+
+const recipeModel = new mongoose.model('myRecipes', recipeSchema);
+
+app.get('/api/recipes', async (req, res) => {
+    const recipes = await recipeModel.find({}); 
+    res.status(200).json({ recipes });
+});
+
+app.get('/api/recipe/:id', async (req, res) => {
+    const recipe = await recipeModel.findById(req.params.id);
+    res.json(recipe);
+});
+
+app.put('/api/recipe/:id', async (req, res) => {
+    let recipe = await recipeModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.send(recipe);
+});
+
+app.post('/api/recipes', async (req, res) =>{
+    console.log("Recipes: "+req.body.title);
+    const { title, year, poster } = req.body;
+    const newRecipe = new recipeModel({ title, year, poster });
+    await newRecipe.save();
+    res.status(201).json({ message: 'Recipe created successfully', recipe: newRecipe });
+})
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.delete('/api/recipe/:id', async (req, res) => {
+  
+    console.log('Deleting recipe with ID:', req.params.id);
+    const recipe = await recipeModel.findByIdAndDelete(req.params.id);
+    res.status(200).send({ message: "Recipe deleted successfully", recipe }); 
 });
