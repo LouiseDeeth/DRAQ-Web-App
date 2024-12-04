ad4302757cf18698949bc8024e23eca1ee1710af
Louise Deeth <louisedeeth@gmail.com>
2024-12-04 22:37:38 +0000

    Delete content

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Content from './components/Content';
 import Read from './components/Read';
 import Create from './components/Create';
 import Edit from './components/Edit';
@@ -13,7 +12,7 @@ function App() {
       <Header />
       <NavigationBar />
       <Routes>
-        <Route path="/Home" element={<Content />} />
+        <Route path="/Home" element={<Read />} />
         <Route path="/Read" element={<Read/>} />
         <Route path="/Create" element={<Create />} />
         <Route path='/Edit/:id' element={<Edit />} />
