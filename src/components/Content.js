56d8116f118c345ec65121b8e88cd5d02a8ef1f0
Louise Deeth <louisedeeth@gmail.com>
2024-12-07 14:23:33 +0000

    Added back in Content page

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div>
+            <h1>Welcome to te Recipe App</h1>
+        </div>
+    );
+}
+
+export default Content;
